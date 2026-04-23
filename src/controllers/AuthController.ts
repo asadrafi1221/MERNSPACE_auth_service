c22c985f1cdd965d68f0aa277d31b9012f9a66b1
asadrafi1221 <157923762+asadrafi1221@users.noreply.github.com>
2026-04-24 02:14:29 +0500

    auth error handling and logger

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,24 +1,39 @@
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { RegisterUserRequest } from '../types';
 import { UserService } from '../services/UserService';
+import { Logger } from 'winston';
 
 export class AuthController {
     userService: UserService;
-    constructor(userService: UserService) {
+    constructor(
+        userService: UserService,
+        private logger: Logger,
+    ) {
         this.userService = userService;
     }
-    async register(req: RegisterUserRequest, res: Response) {
+    async register(
+        req: RegisterUserRequest,
+        res: Response,
+        next: NextFunction,
+    ) {
         const { firstName, lastName, email, password } = req.body;
 
-        const user = await this.userService.create({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
+        try {
+            const user = await this.userService.create({
+                firstName,
+                lastName,
+                email,
+                password,
+            });
 
-        res.status(201).send({
-            id: user.id,
-        });
+            this.logger.info('User has been created', { id: user.id });
+
+            res.status(201).send({
+                id: user.id,
+            });
+        } catch (err) {
+            next(err);
+            return;
+        }
     }
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,24 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Repository } from 'typeorm';
 import { User } from '../entity/User';
 import { UserData } from '../types';
+import createHttpError from 'http-errors';
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
     async create({ firstName, lastName, email, password }: UserData) {
-        const user = await this.userRepository.save({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
-        return user;
-    }
+        try {
+            const user = await this.userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password,
+            });
 
-    async getUserById(id: number) {
-        const user = await this.userRepository.findOne({
-            where: { id },
-        });
-        return user;
+            return user;
+        } catch (err) {
+            const error = createHttpError(
+                500,
+                'Failed to store the user in database ',
+            );
+            throw error;
+        }
     }
 }
