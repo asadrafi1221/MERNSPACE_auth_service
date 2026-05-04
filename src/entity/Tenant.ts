d4f69bbfc742712028d26179ddc28c380daafa5f
asadrafi1221 <157923762+asadrafi1221@users.noreply.github.com>
2026-05-05 00:56:44 +0500

    added tenant table

---
+++ src/entity/Tenant.ts
@@ -0,0 +1,27 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({
+    name: 'tenants',
+})
+export class Tenant {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column('varchar', { length: 100 })
+    name!: string;
+
+    @Column('varchar', { length: 100 })
+    adress!: string;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+}
---
+++ src/migration/1777924101264-create_tenants.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTenants1777924101264 implements MigrationInterface {
+    name = 'CreateTenants1777924101264';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `CREATE TABLE "tenants" ("id" SERIAL NOT NULL, "name" character varying(100) NOT NULL, "adress" character varying(100) NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_53be67a04681c66b87ee27c9321" PRIMARY KEY ("id"))`,
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "tenants"`);
+    }
+}
